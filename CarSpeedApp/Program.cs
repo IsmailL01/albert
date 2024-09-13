ad7d607e31a9e4f6d99207523ddaabdbadbd119c
Ismail <hummetzadeismail@gmail.com>
2024-09-13 14:57:20 +0300

    commit

--- CarSpeedApp/Program.cs
+++ CarSpeedApp/Program.cs
@@ -2,10 +2,10 @@
 
 public class HelloWorld
 {
-    public static void Main(string[] args)
+    public static void Main()
     {
         string str;
-        int V1, V2, T, s;
+        int V1, V2, T, s, T2;
         Console.WriteLine ("Enter V1");
         str=Console.ReadLine();
         V1=Convert.ToInt32(str);
@@ -15,7 +15,10 @@ public class HelloWorld
         Console.WriteLine ("Enter time");
         str=Console.ReadLine();
         T=Convert.ToInt32(str);
-        s=(V1+V2)*T;
+        Console.WriteLine("Введите изначальное расстояние между автомобилями");
+        str=Console.ReadLine();
+        T2=Convert.ToInt32(str);
+        s=(V1+V2)*T+T2;
 
         
         Console.WriteLine ("The distance between the machines ="+s +"km"); 
